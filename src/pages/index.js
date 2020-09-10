86048f150a8b13bed02b55a716bc5de2bbdfbd3b
ksyasuda <ksyasuda@umich.edu>
2020-09-10 16:52:11 -0400

    Update some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,63 @@ import SEO from "../components/seo"
 import Russell from "../components/russell"
 import Charlie from "../components/charlie"
 import classes from "./blog.module.css"
+import { graphql, Link } from "gatsby"
 class IndexPage extends Component {
 	state = {
 		charlie: false,
+		posts: [],
 	}
 
 	onCharlieHandler = () => {
 		this.setState({ charlie: !this.state.charlie })
+	}
+
+	componentDidMount = () => {
+		const { data } = this.props
+		let posts = [...this.state.posts]
+		data.allMarkdownRemark.edges.map(node => {
+			// console.log(node)
+			node = node.node
+			posts.push(
+				<Link
+					key={node.id}
+					to={node.fields.slug}
+					style={{
+						backgroundColor: "dodgerblue",
+					}}
+					className={classes.link}
+				>
+					<div
+						key={node.id}
+						style={{ backgroundColor: "dodgerblue" }}
+						className={classes.postContainer}
+					>
+						<h3
+							className={classes.Title}
+							style={{ marginBottom: "2px" }}
+						>
+							{node.frontmatter.title}
+							{<br />}
+							<span
+								style={{
+									color: "black",
+									fontSize: "small",
+									textDecoration: "none",
+								}}
+							>
+								- {node.frontmatter.date} |{" "}
+								<span className={classes.Subject}>
+									{node.frontmatter.subject}
+								</span>{" "}
+							</span>
+							<hr className={classes.Line} />
+						</h3>
+						<p className={classes.Body}>{node.excerpt}</p>
+					</div>
+				</Link>
+			)
+		})
+		this.setState({ posts: posts })
 	}
 
 	render() {
@@ -36,9 +86,32 @@ class IndexPage extends Component {
 					</button>
 					{this.state.charlie ? <Charlie /> : <Russell />}
 				</div>
+				{this.state.posts.length > 0 ? this.state.posts : null}
 			</Layout>
 		)
 	}
 }
+
+export const query = graphql`
+	query {
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date
+						subject
+					}
+					excerpt
+					fields {
+						slug
+					}
+				}
+			}
+		}
+	}
+`
 
 export default IndexPage
