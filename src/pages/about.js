f9fc0d5bf85518ed01bf7dd3d0ce7824d49290d6
ksyasuda <ksyasuda@umich.edu>
2020-09-13 00:51:40 -0400

    Update about page not done yet

--- src/pages/about.js
+++ src/pages/about.js
@@ -46,7 +46,46 @@ export default function about() {
 						different topics in computer science including Bash
 						scripting and Python.
 					</p>
-					<p className={classes.AboutParagraph}></p>
+					<p className={classes.AboutParagraph}>
+						This past summer, as a result of Covid-19, I was unable
+						to participate in a summer internship and as a result I
+						decided to spend the summer learning web development,
+						and specifically React.js. I have always had an interest
+						in web development, but lacked the time and motivation
+						to commit myself to learning how to build websites. This
+						is in part due to the fact that for three years at
+						Michigan, the only programs that I wrote were C++
+						command line applications. I also knew that C++ is not a
+						language that people generally create websites with
+						(although you can do it). Because of those two reasons I
+						decided to branch out, try something new, and experience
+						some new software. The only question was: React,
+						Angular, or Vue. After doing some initial research I
+						quickly decided not to learn Angular becuase of the need
+						to learn typescript as well. I do plan on learning
+						typescript eventually for use in React applications,
+						however, because I had no prior experience with HTML,
+						CSS, or JavaScript, I decided hold off on learning
+						typescript until after I learned JavaScript. The reason
+						that I went with React over Vue was simply because React
+						is larger and has more users than Vue. The advantage of
+						this is that there are many third-party libraries,
+						components, and tools that are easy to find and
+						integrate into your own project. Another advantage of
+						having a large user base is that any problem you come
+						across is likely to have been seen and solved by someone
+						on the internet before. As a result, I spent the entire
+						summer learning about something in react like hooks or
+						API calls, then creating a small react app using what I
+						had learned. Eventually, I put it all together to create
+						this website, which is the culmination of three and a
+						half projects I worked on this summer (I will go into
+						more detail about why three and a half in a future blog
+						post). I used the plethora of free material available
+						online to learn react as well as reading through the
+						official documentation, something that I became very
+						farmiliar with during this summer
+					</p>
 				</div>
 				{/* <Neofetch style={{ width: "60%", left: "20%" }} /> */}
 			</div>
